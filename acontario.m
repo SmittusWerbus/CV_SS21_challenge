0eced41977f551c9e843fd589e23a2506d1f760c
MaxKrasch <62376024+MaxKrasch@users.noreply.github.com>
2021-07-04 20:21:33 +0200

    function dummies erstllt

---
+++ acontario.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = acontario(inputArg1,inputArg2)
+%ACONTARIO Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
