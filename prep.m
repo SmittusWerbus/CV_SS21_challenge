c37bcf8e370e15217c9c7bbc04a6ad6b9379e917
SmittusWerbus <67009081+SmittusWerbus@users.noreply.github.com>
2021-07-07 19:00:55 +0200

    TestFertig auf Module gesplitted

--- prep.m
+++ prep.m
@@ -9,4 +9,7 @@ I2_raw = rgb2gray(imread(fullfile('images','Frauenkirche','2015_08.jpg')));
 I1 = adapthisteq(I1_raw,'NumTiles',[10 10]);
 I2 = adapthisteq(I2_raw,'NumTiles',[10 10]);
 
+I1 = single(I1);
+I2 = single(I2);
+
 end
--- sift.m
+++ sift.m
@@ -1,7 +1,25 @@
-function [outputArg1,outputArg2] = sift(inputArg1,inputArg2)
+function [f1, d1, f2, d2, Keypoints_1, Keypoints_2] = sift(I1, I2)
 %SIFT Summary of this function goes here
 %   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+
+
+% These parameters limit the number of features detected
+peak_thresh = 0; % increase to limit; default is 0
+edge_thresh = 10; % decrease to limit; default is 10
+
+[f1,d1] = vl_sift(I1,'PeakThresh', peak_thresh, 'edgethresh', edge_thresh );
+
+Keypoints_1 = [f1(1,:);f1(2,:)];
+
+% These parameters limit the number of features detected
+
+peak_thresh = 0; % increase to limit; default is 0
+edge_thresh = 10; % decrease to limit; default is 10
+[f2,d2] = vl_sift(I2, 'PeakThresh', peak_thresh, 'edgethresh', edge_thresh );
+
+
+Keypoints_2 = [f2(1,:);f2(2,:)];
+
+
 end
 
