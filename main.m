a78275298fac1e9037e345dae2c6ea4d808f9ee8
SmittusWerbus <67009081+SmittusWerbus@users.noreply.github.com>
2021-07-07 19:47:36 +0200

    Debuggen von main

--- main.m
+++ main.m
@@ -6,22 +6,27 @@
 
 % Feature detection 
 
-[points_1_res, points_2_res] = surf(I1, I2);
+%[points_1_res, points_2_res] = surf(I1, I2);
 %[points_1_res, points_2_res] = orb(I1, I2);  % nicht wundern orb braucht ewig deswegen für demo mal surf
+[f1, d1, f2, d2, Keypoints_1, Keypoints_2] = sift(I1, I2);
+
 
 % Feature matching
 
-[matchedPoints_1, matchedPoints_2] = matching(I1,I2, points_1_res, points_2_res);
+%[matchedPoints_1, matchedPoints_2] = matching(I1,I2, points_1_res, points_2_res);
+[f1match, f2match, d1match, d2match, allX, allY, allScales, allAngs, matches] = matching(f1, f2, d1, d2);
+
 
 
 % Ransac'en
 
-[f_LMedS, inliers] = ransac(matchedPoints_1, matchedPoints_2);
+%[f_LMedS, inliers] = ransac(matchedPoints_1, matchedPoints_2);
+[Inliers_1, Inliers_2] = ransac(f1match, f2match, allX, allY, allScales, allAngs, matches);
 
 
 % Plotten von bereinigten matches
 
-plot_matches(I1,I2, matchedPoints_1, matchedPoints_2, inliers);
+%plot_matches(I1,I2, matchedPoints_1, matchedPoints_2, inliers);
 
 
 % Der Rest?
