4e6fd6aed69a97fcef01280934fe7d0b5065ff8b
SmittusWerbus <67009081+SmittusWerbus@users.noreply.github.com>
2021-07-07 20:38:17 +0200

    Ransac in Hough binning umbenannt weil wir jetzt eine andere Methode erstmal nutzen

--- main.m
+++ main.m
@@ -1,4 +1,10 @@
 
+% Init VLfeat Toolbox
+
+%run(fullfile('CV_SS21_challenge', 'VLFEATROOT', 'toolbox', 'vl_setup.m'));
+%run('CV_SS21_challenge/VLFEATROOT/toolbox/vl_setup')
+
+
 % Pre processing der Bilder
 
 [I1, I2] = prep;
@@ -8,6 +14,12 @@
 
 %[points_1_res, points_2_res] = surf(I1, I2);
 %[points_1_res, points_2_res] = orb(I1, I2);  % nicht wundern orb braucht ewig deswegen für demo mal surf
+
+ 
+% The matrix f has a column for each frame. A frame is a disk of center f(1:2), scale f(3) and orientation f(4)
+% The matrix d consists of a 128 (descriptor) x #feature/frame arrangement  
+% The Keypoints consist of the feature location x & y
+
 [f1, d1, f2, d2, Keypoints_1, Keypoints_2] = sift(I1, I2);
 
 
@@ -18,11 +30,12 @@
 
 
 
-% Ransac'en
+% Hough Transform binning -> ersetzt erstmal unseren Ransac
+% Inliers bezeichnet die matched keypoints nach dem binning 
 
 %[f_LMedS, inliers] = ransac(matchedPoints_1, matchedPoints_2);
-[Inliers_1, Inliers_2] = ransac(I1, I2,f1match, f2match, allX, allY, allScales, allAngs, matches);
-
+[Inliers_1, Inliers_2] = hough_binning(I1, I2, f1match, f2match, allX, allY, allScales, allAngs, matches);
+    
 
 % Plotten von bereinigten matches
 
