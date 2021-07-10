15cded1ce778a9d60fd1d1d2d0a4ee98ab71aed1
SmittusWerbus <67009081+SmittusWerbus@users.noreply.github.com>
2021-07-10 19:45:10 +0200

    NEWEST

--- showChanges.m
+++ showChanges.m
@@ -1,4 +1,4 @@
-function [pic] = showChanges(pic)
+function [pic,Legend] = showChanges(pic)
 orange = [255, 127, 0];
 light_green = [0, 255, 0];
 dark_green = [0, 100, 0];
@@ -7,7 +7,9 @@ purple = [160, 32, 240];
 pink = [255, 105, 180];
 chocolate = [139, 169, 19];
 color = {"red", orange, "yellow", light_green, dark_green, light_blue, "blue", purple, pink, chocolate, "black", "white"};
+color_index=1;
 for i=2:(length(pic))
+    
     j = length(pic)-i+2;
     cj = pic{1,j}.SURF.changed_points;
 %     if j>2
@@ -32,15 +34,21 @@ for i=2:(length(pic))
 %     end
     
     pic{1,j}.SURF.only_change = cj;
-    I = insertMarker(pic{1,1}.picture, cj, 'color', color{i-1});
+    I = insertMarker(pic{1,1}.picture, cj, 'color', color{color_index});
     pic{1,j}.SURF.change_picture = I;
+    color_index=color_index+1;
 end
-
+saved_color_index=color_index;
+color_index=1;
 pic{1,1}.SURF.time_change_pic = pic{1,1}.picture;
+Legend{1}=datestr(pic{1,1}.date);
 for i=2:length(pic)
-    pic{1,i}.SURF.time_change_pic = insertMarker(pic{1,i-1}.SURF.time_change_pic, pic{1,i}.SURF.only_change, 'color', color{i-1});
+    Legend{i}=datestr(pic{1,i}.date);
+    pic{1,i}.SURF.time_change_pic = insertMarker(pic{1,i-1}.SURF.time_change_pic, pic{1,i}.SURF.only_change, 'color', color{saved_color_index-color_index});
     pic{1,i}.SURF.change_color = color{i-1};
 %     imshow(pic{1,i}.SURF.time_change_pic);
+    color_index=color_index+1;
 end
+
 end
 
