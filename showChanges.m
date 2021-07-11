bac67dfa763703ae7c882ad227450c4044ca2102
MaxKrasch <62376024+MaxKrasch@users.noreply.github.com>
2021-07-11 19:10:40 +0200

    nice version

--- showChanges.m
+++ showChanges.m
@@ -1,12 +1,16 @@
 function [pic,Legend] = showChanges(pic)
+red = [255, 0, 0];
 orange = [255, 127, 0];
-light_green = [0, 255, 0];
-dark_green = [0, 100, 0];
+yellow = [255, 255, 0];
+light_green = [0, 100, 0];
+dark_green = [0, 255, 0];
 light_blue = [135, 206, 250];
+blue = [0, 0, 255];
 purple = [160, 32, 240];
 pink = [255, 105, 180];
 chocolate = [139, 169, 19];
-color = {"red", orange, "yellow", light_green, dark_green, light_blue, "blue", purple, pink, chocolate, "black", "white"};
+color = {red, orange, yellow, light_green, dark_green, light_blue, blue, purple, pink, chocolate, [25, 55, 200], [125, 155, 20], randi([0,255],1,3), randi([0,255],1,3), randi([0,255],1,3), randi([0,255],1,3), randi([0,255],1,3), randi([0,255],1,3), randi([0,255],1,3), randi([0,255],1,3)};
+color_hist = {randi([0,255],1,3), randi([0,255],1,3), randi([0,255],1,3), randi([0,255],1,3), randi([0,255],1,3), randi([0,255],1,3), randi([0,255],1,3), randi([0,255],1,3), [125, 155, 20], [25, 55, 200], chocolate, pink, purple, blue, light_blue, dark_green, light_green, yellow, orange, red};
 color_index=1;
 for i=2:(length(pic))
     
@@ -43,6 +47,8 @@ color_index=1;
 pic{1,1}.SURF.time_change_pic = pic{1,1}.picture;
 Legend{1}=datestr(pic{1,1}.date);
 pic{1,1}.Histo.Data=[];
+pic{1,1}.Histo.only_Data=[];
+pic{1,1}.SURF.change_color = [255, 255, 255];
 for i=2:length(pic)
     Legend{i}=datestr(pic{1,i}.date);
     pic{1,i}.SURF.time_change_pic = insertMarker(pic{1,i-1}.SURF.time_change_pic, pic{1,i}.SURF.only_change, 'color', color{saved_color_index-color_index});
@@ -50,7 +56,8 @@ for i=2:length(pic)
 %     imshow(pic{1,i}.SURF.time_change_pic);
     color_index=color_index+1;
     pic{1,i}.Histo.Data=[pic{1,i-1}.Histo.Data,(i-1)*ones(1,length(pic{1,i}.SURF.changed_points))];
+    pic{1,i}.Histo.only_Data=(i-1)*ones(1,length(pic{1,i}.SURF.changed_points));
 end
-
+pic{1,1}.color_hist = color_hist;
 end
 
